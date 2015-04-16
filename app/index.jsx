973c34374896b8ce64b4aafa968d4a90c5d1f3e9
Brad Daily <brad@bradleyboy.com>
2015-04-16 15:19:50 -0500

    Add babel/polyfill example
    Fixes #13

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,11 @@
 // to get around CSS order randomness in webpack.
 require('./css/base.sass');
 
+// Some ES6+ features require the babel polyfill
+// More info here: https://babeljs.io/docs/usage/polyfill/
+// Uncomment the following line to enable the polyfill
+// require("babel/polyfill");
+
 import React from 'react';
 import Application from './components/Application';
 
